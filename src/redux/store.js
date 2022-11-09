2103ec50c38a5c2794b9e949f8fbd62e5755e744
skalskaga <agnieszkaskalska93@gmail.com>
2022-11-09 21:41:18 +0100

    Fix issue with redux not updating store

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,7 +13,6 @@ const reducer = combineReducers(subreducers);
 const store = createStore(
   reducer,
   initialState,
-
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
