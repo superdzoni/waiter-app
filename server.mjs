21151665afdabb39103e8e71783349f261a452e9
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-11-09 11:04:47 +0100

    Change server.js to server.mjs

--- server.mjs
+++ server.mjs
@@ -1,5 +1,4 @@
-/* global require, process */
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
 const server = jsonServer.create();
 const router = jsonServer.router('build/db/app.json');
 const middlewares = jsonServer.defaults({
